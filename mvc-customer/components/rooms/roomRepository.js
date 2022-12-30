6d6656ceb4abc71bc54ae7bdef4997677ed7a100
qthang1802 <qthangmapdit1802@gmail.com>
2022-12-30 14:15:20 +0700

    shipping detail

--- mvc-customer/components/rooms/roomRepository.js
+++ mvc-customer/components/rooms/roomRepository.js
@@ -1,33 +1,33 @@
 const db = require('../../db');
 
 exports.getAll = async () => {
-  const result =  await db.connection.execute('SELECT * FROM room;');
+  const result =  await db.connection.execute('SELECT * FROM roomdetail;');
   return result[0];
 }
 
 
 
 exports.search = async(RoomName) => {
-  const result = await db.connection.execute("select * from room where RoomName like ?", [`%${RoomName}%`]);
+  const result = await db.connection.execute("select * from roomdetail where RoomName like ?", [`%${RoomName}%`]);
   return result[0];
 }
 
 
 exports.filterByCategory = async(Category) => {
-  const result = await db.connection.execute("select * from room where Category like ?",[Category]);
+  const result = await db.connection.execute("select * from roomdetail where Category like ?",[Category]);
   return result[0];
 }
 
 
 
 exports.sortByPriceAsc = async() => {
-  const result = await db.connection.execute("select * from room order by Price asc");
+  const result = await db.connection.execute("select * from roomdetail order by Price asc");
   return result[0];
 }
 
 
 exports.sortByPriceDesc = async() => {
-  const result = await db.connection.execute("select * from room order by Price desc");
+  const result = await db.connection.execute("select * from roomdetail order by Price desc");
   return result[0];
 
 }
--- mvc-customer/routes/index.js
+++ mvc-customer/routes/index.js
@@ -6,8 +6,10 @@ const roomController = require('../components/rooms/mainPageController');
 
 
 router.get('/',roomController.getHomePage);
+
 router.get('/shop', roomController.lists);
-//router.get('/:RoomID', roomController.details);
+router.get('/:RoomID', roomController.details);
+
 router.get('/:slug', roomController.link_to);
 
 module.exports = router;
