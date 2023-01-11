c039cc61cd8970b6e444c6cb8332c9a640b05ea7
antruong <20127104@student.hcmus.edu.vn>
2023-01-11 22:00:53 +0700

    change register

--- mvc-customer/components/auth/authController.js
+++ mvc-customer/components/auth/authController.js
@@ -8,42 +8,38 @@ const ajv = new Ajv();
 addformats(ajv);
 
 exports.showRegistrationForm = (req, res) => {
-  res.render('auth/register', {layout:'layout_register.hbs'});
+  res.render('auth/register', { layout: 'layout_register.hbs' });
 };
 
-
 exports.register = async (req, res) => {
-    const schema = {
-      type: 'object',
-      properties: {
-        'full-name': { type: 'string', 'minLength': 1 },
-        email: { type: 'string', format: 'email' },
-        password: { type: 'string', 'minLength': 6 },
-      },
-      required: ['full-name', 'email', 'password'],
-      additionalProperties: false,
-    };
-    // syntax validation
-    if (!ajv.validate(schema, req.body)) {
-      res.render('auth/register', { error: 'Invalid INput!', layout:'layout_register.hbs'} );
-      return;
-    }
-    const { 'full-name': fullName, email, password } = req.body;
-    try {
-      await authService.register(fullName, email, password);
-    } catch (e) {
-      res.render('auth/register', { error: e.message,layout:'layout_register.hbs' });
-      return;
-    }
-    res.redirect('/auth/login');
+  const schema = {
+    type: 'object',
+    properties: {
+      'full-name': { type: 'string', 'minLength': 1 },
+      email: { type: 'string', format: 'email' },
+      password: { type: 'string', 'minLength': 6 },
+    },
+    required: ['full-name', 'email', 'password'],
+    additionalProperties: false,
   };
-
+  if (!ajv.validate(schema, req.body)) {
+    res.render('auth/register', { error: 'Invalid INput!', layout: 'layout_register.hbs' });
+    return;
+  }
+  const { 'full-name': fullName, email, password } = req.body;
+  try {
+    await authService.register(fullName, email, password);
+  } catch (e) {
+    res.render('auth/register', { error: e.message, layout: 'layout_register.hbs' });
+    return;
+  }
+  res.redirect('/auth/login');
+};
 
 
 exports.showLoginForm = (req, res) => {
-  res.render('auth/login', {layout:'layout_register.hbs'});
+  res.render('auth/login', { layout: 'layout_register.hbs' });
 };
-
 
 exports.logout = (req, res) => {
   req.logout(function (err) {
